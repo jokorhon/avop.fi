e48de3a551f9eb431c8bc2c8ab95c376aede0c47
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-02-05 16:18:44 +0200

    AV-9: added theme-image

--- frontend/src/components/content/Content.js
+++ frontend/src/components/content/Content.js
@@ -17,24 +17,33 @@ export default class Content extends React.Component {
   render() {
     return (
       <section>
-        <div className="row content-section">
-
-          <div className="one-half column">
-            <Translate component="h4" content="content.kysely.information"/>
-            <Translate component="p" content="content.kysely.about1"/>
-            <Translate component="p" content="content.kysely.about2"/>
-          </div>
-
-          <div className="one-half column">
-            <div id="haka">
-              <Translate component="h4" content="content.login.header"/>
-              <Translate component="p" content="content.login.description"/>
-              <HakaLogin></HakaLogin>
+        <div className="row">
+          <div className="u-full-width">
+            <div id="themeimage">
+              <div id="haka-login">
+                <Translate component="h4" content="content.login.header"/>
+                <Translate component="p" content="content.login.description"/>
+                <HakaLogin></HakaLogin>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="separator"></div>
+        <div className="row content-section">
+          <div className="u-full-width">
+            <Translate component="h4" content="content.kysely.information"/>
+          </div>
+        </div>
+
+        <div className="row content-section">
+          <div className="one-half column">
+            <Translate component="p" content="content.kysely.about1"/>
+          </div>
+
+          <div className="one-half column">
+            <Translate component="p" content="content.kysely.about2"/>
+          </div>
+        </div>
 
         <div className="content-section">
           <div className="u-full-width ">
