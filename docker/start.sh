73e7ce787ead9fed1bc08356adc56cdc249b5bfd
OscarP <oscar.perez@gofore.com>
2016-03-01 10:25:28 +0200

    Kiss approach for the moment to make docker work. no need to get fancy

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-AVOPFIPATH=/home/mostro/projects/avop.fi/ansible
-docker rm -f -v apache-temp
+AVOPFIPATH=/home/mostro/projects/avop.fi
+docker rm -f -v avop-temp
 docker run \
   -v $AVOPFIPATH/ansible:/ansible:ro \
   -w /ansible \
-  --name=apache-temp \
+  --name=avop-temp \
   ansible/centos7-ansible:stable \
   ansible-playbook -i hosts/dev.txt setup.yml --vault-password-file=vault.password
-&& docker commit apache-temp apache
-docker rm -f -v apache-temp
+&& docker commit avop-temp avop
+docker rm -f -v avop-temp
