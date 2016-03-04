25012203f9f0b0094f4f2bc8c36ce54bd8b2ec91
Timo Hanhirova <timo.hanhirova@arcusys.fi>
2016-03-04 16:18:01 +0200

    Fix target point to test server

--- frontend/src/config/dist.js
+++ frontend/src/config/dist.js
@@ -4,7 +4,7 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',
-  loginTarget: '?target=https://www.avop.fi/user'
+  loginTarget: '?target=https://avopfitest.csc.fi/user'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
