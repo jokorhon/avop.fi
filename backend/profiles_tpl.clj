6f7933112380e99cafd7eca2ba85a66be15d65e9
Timo Hanhirova <timo.hanhirova@arcusys.fi>
2016-03-07 19:46:18 +0200

    Re-add profiels_tpl.clj

---
+++ backend/profiles_tpl.clj
@@ -0,0 +1,12 @@
+{:profiles/dev  {:env {
+   :is-dev true
+   :virta-tunnus "avopfi"
+   :virta-jarjestelma "avopfi"
+   :virta-salaisuus "salaisuus"
+   :virta-url "http://virtawstesti.csc.fi/luku/opiskelijatiedot.wsdl"
+   :opintopolku-base-url "https://testi.virkailija.opintopolku.fi/"
+   :arvo-api-url "http://avoptest.csc.fi/api/"
+   :arvo-answer-url "http://avopvastaustest.csc.fi/"
+   :database-url "jdbc:postgresql://192.168.99.100/avopfi?user=avopfi&password=avopfi"}}
+ :profiles/test {:env {
+ 	:database-url "jdbc:postgresql://192.168.99.100/avopfi_test?user=avopfi&password=avopfi"}}}
