fbd3d8f85e98eb576ed338fc7716adeb7a198386
Timo Hanhirova <timo.hanhirova@arcusys.fi>
2016-04-07 14:21:24 +0300

    Change vastaussnap address to profiles t/l

--- backend/profiles_tpl.clj
+++ backend/profiles_tpl.clj
@@ -6,8 +6,14 @@
    :virta-url "http://virtawstesti.csc.fi/luku/opiskelijatiedot.wsdl"
    :opintopolku-base-url "https://testi.virkailija.opintopolku.fi/"
    :arvo-api-url "http://avopsnap.csc.fi/api/public/luovastaajatunnus"
-   :arvo-answer-url "http://avopvastaustest.csc.fi/"
+   :arvo-answer-url "http://avopvastaussnap.csc.fi/"
    :arvo-jwt-secret	"secret"
    :database-url "jdbc:postgresql://192.168.99.100/avopfi?user=avopfi&password=avopfi"}}
  :profiles/test {:env {
  	:database-url "jdbc:postgresql://192.168.99.100/avopfi_test?user=avopfi&password=avopfi"}}}
+
+
+   
+Näytä kartta
+Futurum Music Bar
+Zborovská
