3ba0022f1c7288d283b77591db92198d3bed93ca
Timo Hanhirova <timo.hanhirova@arcusys.fi>
2016-04-07 21:15:54 +0300

    Add loading message

--- frontend/src/components/user/userprofile.js
+++ frontend/src/components/user/userprofile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { browserHistory } from 'react-router'
+import { browserHistory } from 'react-router';
 import Translate from 'react-translate-component';
 import TranslateProperty from '../common/translateproperty';
 
