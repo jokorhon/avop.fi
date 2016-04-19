da82e532e0ce88ebedbecefb71569b584cd42c5e
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-04-19 10:09:31 +0300

    IE doesn't support startsWith. Use indexOf instead

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -22,9 +22,9 @@ class AppComponent extends React.Component {
   }
 
   setLocale() {
-    if (this.props.params.lang.startsWith('fi')) {
+    if (this.props.params.lang.indexOf('fi') === 0 ) {
       counterpart.setLocale('fi');
-    } else if (this.props.params.lang.startsWith('sv')) {
+    } else if (this.props.params.lang.indexOf('sv') === 0 ) {
       counterpart.setLocale('sv');
     } else {
       counterpart.setLocale('en');
