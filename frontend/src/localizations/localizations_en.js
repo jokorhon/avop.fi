7a7faa0a14f233821a4522b5d22f323461bdeead
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-04-19 10:08:21 +0300

    add english localizations

--- frontend/src/localizations/localizations_en.js
+++ frontend/src/localizations/localizations_en.js
@@ -1,34 +1,34 @@
 let localizations_en = {
   header: {
-    logotext: 'Ammattikorkeakoulujen valmistumisvaiheen opiskelijapalaute'
+    logotext: 'Polytechic graduand feedback questionnaire'
   },
   content: {
     kysely: {
-      information: 'IN ENGLISH - Tietoa kyselystä',
-      about1: 'AVOP on Ammattikorkeakoulujen valmistumisvaiheen opiskelijapalautekysely, jossa opiskelija arvioi ja antaa palautetta toteutuneesta koulutuksesta. Kyselyn tuloksia hyödynnetään sekä paikallisesti korkeakoulun oman toiminnan kehittämisessä että valtakunnallisesti koulutuksen ohjauksessa ja rahoituksessa. Kyselyn täyttävät kaikki ammattikorkeakouluista valmistuvat tutkinto-opiskelijat (paitsi Poliisiammattikorkeakoulu).',
-      about2: 'AVOP -kyselymittaristo on syntynyt Ammattikorkeakoulujen rehtorineuvosto Arene ry:n, Suomen opiskelijakuntien SAMOK ry:n, Opiskelun ja koulutuksen tutkimussäätiön (OTUS) ja opetus- ja kulttuuriministeriön (OKM) yhteisenä hankkeena. Kyselymittariston käyttöönotosta ja sen kehittämisestä vastaa Arene ry.',
-      about3: 'AVOP-valtakunnallinen sähköinen palautekysely on osa valtakunnallista Arvo-vaikuttavuustietopalvelua, jonka avulla voidaan kerätä valtakunnallisesti yhteismitallista tietoa koulutuksen vaikuttavuudesta.'
+      information: 'About the questionnaire',
+      about1: 'AVOP - Graduand feedback questionnaire asks graduating students to evaluate and provide feedback on their education. The findings are used locally by institutions to improve their institutional processes and practices and nationally to inform performance-based monitoring and funding. All graduating degree students from universities of applied sciences are requested to fill out the questionnaire (except Police University College).',
+      about2: 'The questionnaire was developed in close cooperation with the Rectors\' Conference of Finnish Universities of Applied Sciences (ARENE), the Union of Students in Finnish Universities of Applied Sciences (SAMOK), Foundation for Research on Studying and Education (OTUS) and the Ministry for Education and Culture. The responsibility for development and implementation of the questionnaire lies with Arene.',
+      about3: 'The electronic AVOP - Graduand feedback questionnaire is part of national of the ARVO – education management information service that enables collection of nationally compatible data on the impact of education.'
     },
     login: {
       header: 'Ammattikorkeakouluopiskelija',
       description: 'Vastaa kyselyyn kirjautumalla oman korkeakoulusi tunnuksilla:'
     },
     tulokset: {
-      header: 'Kyselyn tulokset',
-      results1: 'Ammattikorkeakoulujen henkilökunta voi tarkastella kyselyn tuloksia reaaliaikaisesti Arvo - opetushallinnon vaikuttavuustietopalvelun avulla. Katseluoikeudet (Arvo-katselija) voi anoa Opintopolun virkailijantyöpöydän kautta (Omat tiedot)',
-      results2: 'Kyselyn tuloksista johdettuja tilastoraportteja on avoimesti saatavilla opetushallinnon tilastopalvelu Vipusessa:'
+      header: 'The results',
+      results1: 'The staff of the universities of applied sciences can review the real time survey results by using the Arvo - education management information service with appropriate user rights.',
+      results2: 'The statistics are freely available at the Vipunen statistics services of the educational administration.'
     }
   },
   footer: {
     header: 'Yhteistyössä:',
     arene: {
-      description: 'Ammattikorkeakoulujen rehtorineuvosto Arene ry'
+      description: 'Rectors´Conference of Finnish Universities of Applied Sciences, Arene'
     },
     okm: {
-      description: 'Opetus- ja kulttuuriministeriö'
+      description: 'Ministry for Education and Culture'
     },
     csc: {
-      description: 'CSC - Tieteen tietotekniikan keskus Oy'
+      description: 'Arvo-educational management information series, CSC'
     }
   },
   profiledata: {
